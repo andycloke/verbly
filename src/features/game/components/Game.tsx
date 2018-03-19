fc3d3ef42ae2b2992f137bf9f5b30ad78058c68c
andycloke <apcloke@gmail.com>
2018-03-19 09:40:55 +0000

    Add layout to game container

--- src/features/game/components/Game.tsx
+++ src/features/game/components/Game.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentSend from 'material-ui/svg-icons/content/send';
 
 type Props = {};
 
@@ -7,16 +11,27 @@ import './Game.css';
 
 const Game = ({  }: Props) => {
   return (
-    <Paper>
-      <div className="Game__wrapper">
-        <span>Tense</span>
-        <span>Present</span>
-        <span>5</span>
-        <span>Verb</span>
-        <span>Correr - to run</span>
-        <span>3</span>
-      </div>
-    </Paper>
+    <div className="Game__outer">
+      <Paper>
+        <div className="Game__inner">
+          <span className="Game__innerText--border">Tense</span>
+          <span className="Game__innerText--border Game__innerText--leftAlign Game__tense">
+            Present
+          </span>
+          <span className="Game__innerText--border">Verb</span>
+          <span className="Game__innerText--border Game__innerText--leftAlign Game__verb">
+            Correr - to run
+          </span>
+          <span className="Game__person">Tu</span>
+          <TextField className="Game__input" id="text-field-default" />
+          <div className="Game__submitButtonCell">
+            <FloatingActionButton secondary mini className="Game__submitButton">
+              <ContentSend />
+            </FloatingActionButton>
+          </div>
+        </div>
+      </Paper>
+    </div>
   );
 };
 
