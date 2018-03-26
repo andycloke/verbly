1b47c810e9994f8f27f6ee75089c9d7979686a93
andycloke <apcloke@gmail.com>
2018-03-26 07:48:44 +0100

    Make verbly title a link to home

--- src/features/common/components/AppBar.tsx
+++ src/features/common/components/AppBar.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import MaterialAppBar from 'material-ui/AppBar';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
 import FlatButton from 'material-ui/FlatButton';
 
+import { pathToHome } from '../../../paths';
 import { DispatchProps } from '../containers/AppBar';
 import EndGameButton from '../containers/EndGameButton';
+
+import './AppBar.css';
 
 const AppBar = ({ toggleOptionsMenuOpen }: DispatchProps) => {
   const handleOptionsClick = (e: React.MouseEvent<HTMLButtonElement>): void =>
     toggleOptionsMenuOpen();
   return (
     <MaterialAppBar
-      title="Verbly"
+      className="AppBar__outer"
+      title={<Link to={pathToHome()}>Verbly</Link>}
       iconElementRight={
         <FlatButton
           label="Options"
