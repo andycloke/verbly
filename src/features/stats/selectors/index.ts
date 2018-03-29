76322eb962533a7bcf83ad5037b7ebbe2687d7ed
andycloke <apcloke@gmail.com>
2018-03-29 09:02:16 +0100

    Change selector names

--- src/features/stats/selectors/index.ts
+++ src/features/stats/selectors/index.ts
@@ -5,13 +5,21 @@ export const getStatsSlice = (state: any): any => state.stats;
 export const getTenseSlice = (state: any, tense: string): any =>
   getStatsSlice(state).hasOwnProperty(tense) ? getStatsSlice(state)[tense] : {};
 
-export const getAllVerbsWithStatsForTense = (
+export const getAllVerbsThatHaveStatsForTense = (
   state: any,
   tense: string
 ): string[] => Object.keys(getTenseSlice(state, tense));
 
-export const getVerbTensesStats = (
+export const getStatsForVerbTense = (
   state: any,
   verb: string,
   tense: string
 ): VerbTenseStats => getTenseSlice(state, tense)[verb];
+
+export const getAllVerbsTenseStatsForTense = (
+  state: any,
+  tense: string
+): Array<VerbTenseStats> =>
+  getAllVerbsThatHaveStatsForTense(state, tense).map((verb: string) =>
+    getStatsForVerbTense(state, tense, verb)
+  );
