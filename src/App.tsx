0912ee445944d88192b2988f833c431167b2c4d2
andycloke <apcloke@gmail.com>
2018-04-04 07:22:39 +0100

    Add pageviews for GA

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import OptionsMenuModal from './features/options/containers/OptionsMenuModal';
 import store from './store';
 import './App.css';
 ReactGA.initialize('UA-99771850-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends React.PureComponent {
   render() {
