8773381dd27dc3af84374e8ac9410aa74c29cd93
andycloke <apcloke@gmail.com>
2018-05-03 09:10:16 +0100

    Extract persistConfig as file

---
+++ src/persistConfig.ts
@@ -0,0 +1,8 @@
+import storage from 'redux-persist/lib/storage';
+
+export default {
+  key: 'root',
+  storage,
+  throttle: 5000,
+  blacklist: ['game', 'conjugations']
+};
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,7 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+import persistConfig from './persistConfig';
 import options from './features/options/reducers';
 import peopleInPlay from './features/menu/features/people/reducers';
 import tensesInPlay from './features/menu/features/tenses/reducers';
@@ -20,12 +11,13 @@ import conjugations from './features/conjugations/reducers';
 import stats from './features/stats/reducers';
 import scores from './features/scores/reducers';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  throttle: 5000,
-  blacklist: ['game', 'conjugations']
-};
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
   options,
